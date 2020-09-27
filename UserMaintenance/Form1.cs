b4a5001f0ecdb19c8d449962ceb5fa63954a5286
Fajkusz András <andras.fajkusz@stud.uni-corvinus.hu>
2020-09-27 17:33:08 +0200

    write out

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,9 @@ namespace UserMaintenance {
         {
             InitializeComponent();
             label1.Text = Resource1.FullName; 
-            label2.Text = Resource1.k; 
+             
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -33,6 +35,27 @@ namespace UserMaintenance {
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath; 
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv"; 
+            sfd.DefaultExt = "csv"; 
+            sfd.AddExtension = true; 
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                // Végigmegyünk a hallgató lista elemein
+                foreach (var s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.WriteLine(); 
+                }
+            }
         }
     }
 }
