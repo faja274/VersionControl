602c5f3ba3a0b87fde0ba2afddeb0433327b48e9
Fajkusz András <andras.fajkusz@bcecid.net>
2020-09-27 13:58:23 +0200

    initial commit

--- UserMaintenance/Properties/AssemblyInfo.cs
+++ UserMaintenance/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("36bce1ba-9cd3-4209-9bea-cba7cd7efcf5")]
+[assembly: Guid("076bcf6e-380c-4c76-95d8-135f3d0a892e")]
 
 // Version information for an assembly consists of the following four values:
 //
