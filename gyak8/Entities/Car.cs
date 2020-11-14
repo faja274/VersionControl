47e0b38e9e408528ea24c57d5a88592ec6cdac9e
Fajkusz András <andras.fajkusz@stud.uni-corvinus.hu>
2020-11-14 12:48:13 +0100

    gyak8 toy interface

--- gyak8/Entities/Car.cs
+++ gyak8/Entities/Car.cs
@@ -13,7 +13,7 @@ namespace gyak8.Entities
         protected override void DrawImage(Graphics graphics)
         {
             Image imageFile = Image.FromFile("Images/car.png");
-            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+            graphics.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
